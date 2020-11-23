8918808000591601ed9b3b09a12317c9e6d644c6
Antonia P <direiellie@gmail.com>
2020-11-23 16:57:25 +0100

    Esercizio completo

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+/**
+ * Generare 10 email e stamparle a schermo utilizzando Vue, Axios e il seguente API endpoint boolean:
+https://flynn.boolean.careers/exercises/api/random/mail
+ */
+
+var app = new Vue ({
+
+    el:'#app',
+
+    data: {
+
+        mail_listbox: [],
+},
+
+mounted() {
+
+    const self = this
+
+        for (var i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail') .then(function (reply) {
+
+            self.mail = reply.data.response;
+
+            self.mail_listbox.push(self.mail);
+        });
+
+    }
+
+}
+
+});
